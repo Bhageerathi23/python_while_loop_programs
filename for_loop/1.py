9187cf2057588022a27919141484a6f3eeddae9a
Bhageerathi23 <bhageerathit2002@gmail.com>
2026-03-10 09:48:40 +0530

    Updated python file

---
+++ for_loop/1.py
@@ -0,0 +1,60 @@
+"""for i in range(0, 20):
+    if i%2==0:
+        print(i)"""
+
+
+
+"""marks=[20,40,60,90,70,85]
+count=0
+
+for i in marks:
+    if i%2==0:
+        count+=1
+    print(i)"""
+
+
+"""n=int(input("Enter a number:"))
+
+for i in range(1,n):
+    print("*"*i) #print("*" * n)
+"""
+
+
+
+"""for i in range(10,0,-2):
+    print(i)"""
+"""
+fact=1
+for i in range(1,6):
+    fact=fact*i
+print(fact)"""
+
+
+"""n=int(input("Enter a number:"))
+fact=1
+for i in range(1,n+1):
+    fact=fact*i
+print(f"factorial of a number of{n}", fact)"""
+
+
+"""n = int(input("How many numbers? "))
+count = 0
+
+for i in range(n):
+    num = int(input("Enter number: "))
+    if num > 0:
+        count += 1
+
+print("Positive numbers count =", count)"""
+
+
+
+"""n=int(input("Enter a number:"))
+
+for i in range(n,0,-1): #for i in range(1, n + 1):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()"""
+
+
+
